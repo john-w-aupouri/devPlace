b46fd03c2b7ab0ecaf56b051f566a0eec957409b
john_wirihana_aupouri <johnaupouri963@gmail.com>
2020-12-17 01:02:10 +1300

    made routes and api folders, all routes working

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Routes
+app.use('/api/users', require('./config/routes/api/users'));
+app.use('/api/auth', require('./config/routes/api/auth'));
+app.use('/api/profile', require('./config/routes/api/profile'));
+app.use('/api/posts', require('./config/routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
