853f603d9588d3e13817cf3606582a528ed5be07
john_wirihana_aupouri <johnaupouri963@gmail.com>
2020-12-17 11:52:15 +1300

    able to save Users to database

--- config/db.js
+++ config/db.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+// Connect to MongoDB
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true 
+      useUnifiedTopology: true,
+      useCreateIndex: true,
     });
     console.log('MongoDB connected!');
-  } catch(err) {
+  } catch (err) {
     console.error(err);
     // Exit process with failure
     process.exit(1);
--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false}));
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 
 // Routes
-app.use('/api/users', require('./config/routes/api/users'));
-app.use('/api/auth', require('./config/routes/api/auth'));
-app.use('/api/profile', require('./config/routes/api/profile'));
-app.use('/api/posts', require('./config/routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
