db91e48a4415c3874560b05493606425d26bd294
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-01-01 03:23:56 +1300

    created landing and navbar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import Landing from './components/layout/landing/landing.component';
+import Navbar from './components/layout/navbar/navbar.component';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
   );
-}
+};
 
 export default App;
