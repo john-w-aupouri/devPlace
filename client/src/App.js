bc2c43705ce7d72903de1934b04c5569f6f7159a
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-01-01 04:04:54 +1300

    react router working with nav links and landing buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { Fragment } from 'react';
-import Landing from './components/layout/landing/landing.component';
+import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/navbar/navbar.component';
+import Landing from './components/layout/landing/landing.component';
+import Register from './components/layout/auth/register/register.component';
+import Login from './components/layout/auth/login/login.component';
 import './App.css';
 
 const App = () => {
   return (
     <Fragment>
       <Navbar />
-      <Landing />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
     </Fragment>
   );
 };
