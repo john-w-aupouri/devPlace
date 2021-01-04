d7c1406f70ba86fbe2f0ab565900cdadd67c06ab
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-01-05 10:32:43 +1300

    registering users on the front-end

--- client/src/pages/register.component.jsx
+++ client/src/pages/register.component.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import Alert from '../components/alert.component';
+
 // Actions
 import { setAlert } from '../redux/actions/alert';
 import { register } from '../redux/actions/auth';
@@ -25,7 +25,7 @@ const Register = ({ setAlert, register }) => {
     if (password !== password2) {
       setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('SUCCESS');
+      register({ name, email, password });
     }
   };
   return (
@@ -42,7 +42,6 @@ const Register = ({ setAlert, register }) => {
             name='name'
             value={name}
             onChange={(e) => onChange(e)}
-            required
           />
         </div>
         <div className='form-group'>
@@ -52,7 +51,6 @@ const Register = ({ setAlert, register }) => {
             name='email'
             value={email}
             onChange={(e) => onChange(e)}
-            required
           />
           <small className='form-text' style={{ color: '#f4f4f4' }}>
             This site uses Gravatar so if you want a profile image, use a
@@ -66,7 +64,6 @@ const Register = ({ setAlert, register }) => {
             name='password'
             value={password}
             onChange={(e) => onChange(e)}
-            minLength='6'
           />
         </div>
         <div className='form-group'>
@@ -76,10 +73,8 @@ const Register = ({ setAlert, register }) => {
             name='password2'
             value={password2}
             onChange={(e) => onChange(e)}
-            minLength='6'
           />
         </div>
-        <Alert />
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
