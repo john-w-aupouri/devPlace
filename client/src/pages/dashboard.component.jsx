53287dffa49b4e8ca2223a59b0ec3aaf748f9220
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-02-04 20:15:35 +1300

    getCurrentUser action not working

--- client/src/pages/dashboard.component.jsx
+++ client/src/pages/dashboard.component.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Dashboard = (props) => {
+// Actions
+import { getCurrentProfile } from '../redux/actions/profile';
+
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
   return <div>Dashboard</div>;
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-export default Dashboard;
+const mapStateToProps = (state) => ({
+  auth: state.authReducer,
+  profile: state.profileReducer,
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import alertReducer from './reducers/alert';
 import authReducer from './reducers/auth';
+import profileReducer from './reducers/profile';
 
 export default combineReducers({
   alertReducer,
   authReducer,
+  profileReducer,
 });
