c649ba32c9ec1cb68edeb875b5f2dacac3d6dbff
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-02-07 18:51:57 +1300

    update

--- client/src/pages/dashboard.component.jsx
+++ client/src/pages/dashboard.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -21,22 +21,22 @@ const Dashboard = ({
   return loading && profile === null ? (
     <Spinner />
   ) : (
-    <>
+    <Fragment>
       <h1 className='large text-primary'>Dashboard</h1>
       <p className='lead'>
         <i className='fas fa-user'></i> Welcome {user && user.name}
       </p>
       {profile !== null ? (
-        <> has </>
+        <Fragment> has </Fragment>
       ) : (
-        <>
+        <Fragment>
           <p>You have not setup a profile, please add some info</p>{' '}
           <Link to='/create-profile' className='btn btn-primary my-1'>
             Add profile
           </Link>
-        </>
+        </Fragment>
       )}
-    </>
+    </Fragment>
   );
 };
 
