67bd40690b64605c24472cc805e6ecfac83f18fd
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-02-22 20:20:31 +1300

    deleteAccount not working

--- client/src/components/dashboard/dashboard.component.jsx
+++ client/src/components/dashboard/dashboard.component.jsx
@@ -10,7 +10,7 @@ import Experience from './experience.component';
 import Education from './education.component';
 
 // Actions
-import { getCurrentProfile } from '../../redux/actions/profile';
+import { getCurrentProfile, deleteAccount } from '../../redux/actions/profile';
 
 const Dashboard = ({
   getCurrentProfile,
@@ -34,6 +34,11 @@ const Dashboard = ({
           <DashboardActions />
           <Experience experience={profile.experience} />
           <Education education={profile.education} />
+          <div className='my-2'>
+            <button className='btn btn-danger' onClick={() => deleteAccount()}>
+              <i className='fas fa-user-minus'></i> Delete my account
+            </button>
+          </div>
         </Fragment>
       ) : (
         <Fragment>
@@ -49,6 +54,7 @@ const Dashboard = ({
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired,
 };
@@ -58,4 +64,6 @@ const mapStateToProps = (state) => ({
   profile: state.profile,
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
+  Dashboard
+);
