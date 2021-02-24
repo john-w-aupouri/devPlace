f7133205312e670a8cba670b457fb3e0ebb78ddd
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-02-25 10:32:32 +1300

    posts are in state

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import AddExperience from './components/profile-forms/add-experience.component';
 import Dashboard from './components/dashboard/dashboard.component';
 import Profiles from './components/profiles/profiles.component';
 import Profile from './components/profile/profile.component';
+import Posts from './components/posts/posts.component';
 
 // Pages
 import Landing from './pages/landing.component';
@@ -66,6 +67,7 @@ const App = () => {
               path='/add-experience'
               component={AddExperience}
             />
+            <PrivateRoute exact path='/posts' component={Posts} />
           </Switch>
         </section>
       </Router>
